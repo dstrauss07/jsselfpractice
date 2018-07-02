48052ba12b459125c2d5dfc45fc56f3a7062d028
David Strauss <david_strauss@microcall.com>
2018-07-02 11:22:28 -0400

    match 3 done

---
+++ match3.js
@@ -0,0 +1,7 @@
+var counter=1;
+for(counter;counter<=100;counter++){
+    if(counter % 3 ==0) {
+        console.log("MATCH " + counter +" is divisible by 3!")
+    }  
+    else{ console.log(counter);}
+}
